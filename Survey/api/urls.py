20c756ebfbe756925853937a487f569a7575b88e
RaunakAli <raunak@primenumbers.in>
2023-10-05 02:55:14 +0530

    Added The url Pattern for SurveyDetail

--- Survey/api/urls.py
+++ Survey/api/urls.py
@@ -1,8 +1,9 @@
 from rest_framework import routers
-from .views import SurveyViewSet
-
-router = routers.DefaultRouter()
-router.register(r'surveys', SurveyViewSet)
+from .views import SurveyDetail
+from django.urls import path, include
 
 
-urlpatterns = router.urls
+
+urlpatterns = [
+    path('survey/<int:survey_id>/', SurveyDetail.as_view(), name='survey_detail-list'),
+]
--- Survey/api/views.py
+++ Survey/api/views.py
@@ -1,12 +1,14 @@
 from rest_framework import status
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from .models import SurveyResponse,Survey
 from .serializers import SurveyResponseSerializer,SurveySerializer
 
-class SurveyViewSet(APIView):
-    def get(self, request, survey_id):
+class SurveyDetail(APIView):
+    def get(self, request,survey_id):
+
         user_id = request.user.id
         try:
             response = SurveyResponse.objects.get(survey_id=survey_id, user_id=user_id)
